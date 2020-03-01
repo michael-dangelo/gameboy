1432dd262036037c27afa6efdcc853ad64f50ff1
Michael D'Angelo <mdangelodev@gmail.com>
2020-02-29 19:55:36 -0500

    Render background
    This is a broken first attempt at rendering.
    Lots of bugs but some semblance of the nintendo
    logo scrolls diagonally onto the screen.

--- graphics.c
+++ graphics.c
@@ -5,9 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static SDL_Window *window = NULL;
 static SDL_Surface *screen = NULL;
+static SDL_Renderer *renderer = NULL;
 
 static uint8_t vram[0x2000];
+
+typedef enum {
+    HBLANK,
+    VBLANK,
+    OAM,
+    VRAM
+} Mode;
+
+static Mode mode = OAM;
+static uint16_t clock = 0;
 
 // FF40 - LCD control
 static uint8_t lcdDisplayEnable = 0;
@@ -28,6 +40,9 @@ static uint8_t scrollX;
 // FF44 - LDC Y-Coordinate
 static uint8_t line = 0;
 
+// FF47 - BG Palette Data
+static uint8_t palette = 0;
+
 void Graphics_init(void)
 {
     if (SDL_Init(SDL_INIT_VIDEO))
@@ -36,7 +51,7 @@ void Graphics_init(void)
         exit(1);
     }
     atexit(SDL_Quit);
-    SDL_Window *window = SDL_CreateWindow("Gameboy", 300, 300, 160, 144, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Gameboy", 300, 300, 160, 144, SDL_WINDOW_SHOWN);
     if (!window)
     {
         printf("Failed to create SDL window\n");
@@ -48,23 +63,62 @@ void Graphics_init(void)
         printf("Failed to get window surface\n");
         exit(1);
     }
-    SDL_memset(screen->pixels, 0, screen->h * screen->pitch);
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer)
+    {
+        printf("Failed to create renderer\n");
+        exit(1);
+    }
     SDL_UpdateWindowSurface(window);
 }
 
-typedef enum {
-    HBLANK,
-    VBLANK,
-    OAM,
-    VRAM
-} Mode;
+static uint16_t tileLineAt(uint16_t mapRow, uint16_t rowOffset, uint16_t yOffset)
+{
+    uint16_t addr = mapRow + rowOffset;
+    uint16_t tile = tileDataSelect ? (uint16_t)vram[addr] * 16
+                                   : 0x1000 + ((int16_t)vram[addr] * 16);
+    uint16_t pixels = tile + yOffset;
+    return vram[pixels] + ((uint16_t)vram[pixels + 1] << 8);
+}
 
-static Mode mode = 0;
-static uint16_t clock = 0;
+static uint8_t colorAt(uint16_t tileLine, uint8_t x)
+{
+    uint8_t l = (tileLine >> 8) & 0xFF;
+    uint8_t h = tileLine & 0xFF;
+    uint8_t pixelOffset = 7 - x; // leftmost pixel is 7th bit
+    uint8_t colorIndex = ((l >> pixelOffset) & 1) | (((h >> pixelOffset) & 1) << 1);
+    if (colorIndex > 3)
+    {
+        printf("bad color value");
+        exit(1);
+    }
+    uint8_t color = (palette >> colorIndex) & 3;
+    static uint8_t colors[4] = {255, 192, 96, 0};
+    return colors[color];
+}
 
 static void renderScanline()
 {
-
+    uint16_t tileMap = bgTileMapSelect ? 0x1C00 : 0x1800;
+    uint16_t tileMapRow = tileMap + (((line + scrollY) >> 3) * 32);
+    uint16_t tileMapIndex = scrollX >> 3;
+    uint16_t yOffset = ((line + scrollY) & 7) * 2;
+    uint16_t tileLine = tileLineAt(tileMapRow, tileMapIndex, yOffset);
+    uint8_t x = scrollX & 7;
+    for (uint8_t i = 0; i < 160; i++)
+    {
+        uint8_t color = colorAt(tileLine, x);
+        SDL_SetRenderDrawColor(renderer, color, color, color, 255);
+        SDL_RenderDrawPoint(renderer, i, line);
+        x++;
+        if (x == 8)
+        {
+            x = 0;
+            tileMapIndex++;
+            tileLine = tileLineAt(tileMapRow, tileMapIndex, yOffset);
+        }
+    }
+    SDL_RenderPresent(renderer);
 }
 
 static void step(uint8_t ticks)
@@ -76,8 +130,7 @@ static void step(uint8_t ticks)
             if (clock < 204)
                 return;
             clock = 0;
-            line++;
-            if (line < 143)
+            if (line++ < 143)
                 mode = OAM;
             else
                 mode = VBLANK;
@@ -115,7 +168,8 @@ void Graphics_step(uint8_t ticks)
     while (SDL_PollEvent(&event))
         if (event.type == SDL_QUIT)
             exit(0);
-    step(ticks);
+    if (lcdDisplayEnable)
+        step(ticks);
     // printf("graphics - clock %d mode %d line %d\n", clock, mode, line);
 }
 
@@ -124,7 +178,8 @@ uint8_t Graphics_rb(uint16_t addr)
     uint8_t res = 0;
     if (addr < 0xA000)
     {
-        res = vram[addr - 0x8000];
+        addr -= 0x8000;
+        res = vram[addr];
     }
     switch (addr)
     {
@@ -140,7 +195,7 @@ uint8_t Graphics_rb(uint16_t addr)
             break;
         case 0xFF41:
             printf("unhandled read from GPU status register");
-            exit(0);
+            exit(1);
             break;
         case 0xFF42:
             res = scrollY;
@@ -151,6 +206,10 @@ uint8_t Graphics_rb(uint16_t addr)
         case 0xFF44:
             res = line;
             break;
+        case 0xFF47:
+            printf("reading from bg palette %02x", palette);
+            res = palette;
+            break;
     }
     // printf("reading from gpu addr %02x val %02x\n", addr, res);
     return res;
@@ -160,7 +219,8 @@ void Graphics_wb(uint16_t addr, uint8_t val)
 {
     if (addr < 0xA000)
     {
-        vram[addr - 0x8000] = val;
+        addr -= 0x8000;
+        vram[addr] = val;
     }
     switch (addr)
     {
@@ -176,7 +236,7 @@ void Graphics_wb(uint16_t addr, uint8_t val)
             break;
         case 0xFF41:
             printf("unhandled write to GPU status register");
-            exit(0);
+            exit(1);
             break;
         case 0xFF42:
             scrollY = val;
@@ -186,6 +246,9 @@ void Graphics_wb(uint16_t addr, uint8_t val)
             // Writing to the LCD Y-Coordinate register (line) resets the counter.
             line = 0;
             break;
+        case 0xFF47:
+            palette = val;
+            break;
     }
-    // printf("writing to gpu addr %02x val %02x\n", addr, val);
+    // printf("writing to gpu addr %04x val %02x\n", addr, val);
 }
