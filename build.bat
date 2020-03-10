c2cc8b1030df4712a2392767895ff094b5a584c5
Michael D'Angelo <mdangelodev@gmail.com>
2020-03-10 14:12:20 -0400

    Move from msvc to gcc

--- build.bat
+++
@@ -1,3 +0,0 @@
-if not exist bin\win\ mkdir bin\win
-copy D:\Code\gameboy\lib\SDL\win32\SDL2.dll bin\win\
-cl /W4 /WX /ID:\Code\gameboy\include D:\Code\gameboy\lib\SDL\win32\SDL2.lib main.c memory.c cpu.c graphics.c timer.c /link /out:bin\win\main.exe
