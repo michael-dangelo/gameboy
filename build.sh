9b9dc5c09ade9a7af21f6b8da197cced873cb482
Michael D'Angelo <mdangelodev@gmail.com>
2020-03-10 16:56:30 -0400

    Return gcc exit code from build.sh
    Allows using && to tie the build to a run if successful.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 mkdir -p bin
-gcc -Wall -Wextra -Werror -o bin/main.exe src/*.c -Iinclude/ -Llib/SDL -lSDL2
 cp lib/SDL/SDL2.dll bin
+gcc -Wall -Wextra -Werror -o bin/main.exe src/*.c -Iinclude/ -Llib/SDL -lSDL2
+exit $?
